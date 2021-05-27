b220f2b9bae7f450307258355d54b317cb2c816c
Laura Caroline Ribeiro de Melo <laura.c.r.melo@gmail.com>
2021-05-28 00:32:55 +0200

    Extend Tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,40 @@
+const colors = {
+  primary: '#0069d1',
+  'primary-contrast': '#fff',
+  'primary-dark': '#004cc0',
+  'primary-dark-contrast': '#fff',
+  'primary-light': '#b3d2f1',
+  'primary-light-contrast': '##00366B',
+  accent: '#496683',
+  'accent-contrast': '#fff',
+  'accent-dark': '#3b5268',
+  'accent-dark-contrast': '#fff',
+  'accent-light': '#7c99b6',
+  'accent-light-contrast': '##001F42'
+}
+
 module.exports = {
   theme: {
-    colors: {
-      primary: '#0069d1',
-      'primary-contrast': '#fff',
-      'primary-dark': '#004cc0',
-      'primary-dark-contrast': '#fff',
-      'primary-light': '#b3d2f1',
-      'primary-light-contrast': '##00366B',
-      accent: '#496683',
-      'accent-contrast': '#fff',
-      'accent-dark': '#3b5268',
-      'accent-dark-contrast': '#fff',
-      'accent-light': '#7c99b6',
-      'accent-light-contrast': '##001F42'
-    },
+    colors: colors,
     fontFamily: {},
-    extend: {}
+    extend: {
+      boxShadow: {
+        'card-card-primary': `0px 0px 5px 0px ${colors.primary}`,
+        'card-primary-dark': `0px 0px 5px 0px ${colors['primary-dark']}`,
+        'card-primary-light': `0px 0px 5px 0px ${colors['primary-light']}`,
+        'card-primary-light-contrast': `0px 0px 5px 0px ${colors['primary-light-contrast']}`,
+        'card-accent': `0px 0px 5px 0px ${colors.accent}`,
+        'card-accent-dark': `0px 0px 5px 0px ${colors['accent-dark']}`,
+        'card-accent-light': `0px 0px 5px 0px ${colors['accent-light']}`,
+        'card-accent-light-contrast': `0px 0px 5px 0px ${colors['accent-light-contrast']}`
+      },
+      width: {
+        '45-p': '45%'
+      },
+      minHeight: {
+        80: '20rem'
+      }
+    }
   },
   variants: {
     extend: {}
