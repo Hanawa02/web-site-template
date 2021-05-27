c502bc3920d1005d370e3b12dce45b251ff92e09
Laura Caroline Ribeiro de Melo <laura.c.r.melo@gmail.com>
2021-05-28 00:33:59 +0200

    Add Banner Section

---
+++ components/Home/Banner.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="w-full bg-accent bg-opacity-5 min-h-80 md:h-96 banner-image"
+    :style="style"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    backgroundImageSrc: {
+      type: String
+    }
+  },
+  computed: {
+    style() {
+      if (!this.backgroundImageSrc) {
+        return ''
+      }
+
+      return `background-image:linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("${this.backgroundImageSrc}");`
+    }
+  }
+})
+</script>
+
+<style>
+.banner-image {
+  object-fit: cover;
+  background-size: cover;
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+}
+</style>
---
+++ components/Home/BannerSection.vue
@@ -0,0 +1,78 @@
+<template>
+  <banner
+    class="flex flex-col justify-end"
+    backgroundImageSrc="//images01.nicepage.com/2c/aa/2caae017a61202f87bc36fab57e85a63.png"
+  >
+    <div
+      class="
+        text-primary-contrast
+        default-section-constraints
+        py-8
+        sm:py-12
+        md:py-16
+      "
+    >
+      <div class="font-bold pb-1 text-primary-light text-lg">
+        {{ $t('pages.home.banerSection.pagePreTitle') }}
+      </div>
+      <app-headline :isLeftAligned="true">
+        <h1 class="text-3xl">
+          {{ $t('pages.home.banerSection.pageTitle') }}
+        </h1>
+      </app-headline>
+      <h2 class="text-2xl leading-7">
+        {{ $t('pages.home.banerSection.pageSubTitle') }}
+      </h2>
+      <div class="flex flex-col sm:flex-row mt-4">
+        <button
+          class="
+            w-max
+            px-4
+            py-2
+            mb-3
+            sm:mr-4
+            sm:mb-0
+            bg-primary
+            text-primary-contrast
+            rounded
+            font-semibold
+          "
+        >
+          {{ $t('pages.home.banerSection.coloredButtonText') }}
+        </button>
+        <button
+          class="
+            w-max
+            px-4
+            py-2
+            bg-transparent
+            border-2 border-primary-contrast
+            text-primary-contrast
+            rounded
+            font-semibold
+          "
+        >
+          {{ $t('pages.home.banerSection.transparentButtonText') }}
+        </button>
+      </div>
+    </div>
+  </banner>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import Banner from '~/components/Home/Banner.vue'
+import AppHeadline from '~/components/UI/AppHeadline.vue'
+
+export default Vue.extend({
+  components: {
+    'app-headline': AppHeadline,
+    banner: Banner
+  },
+  props: {},
+  computed: {}
+})
+</script>
+
+<style scoped></style>
